65de1b81890dfddefe75430b03d34722f13e9214
tomiella <tom@pranjic.at>
2025-10-14 20:55:44 +0200

    init project

---
+++ rest/src/test/java/at/bif/swen/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package at.bif.swen.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
